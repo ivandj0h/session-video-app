545beaeb5e4262af0a8697ac431c8da8643ab442
ivandjoh <ivandj0h@users.noreply.github.com>
2025-06-08 04:18:02 +0800

    layout design

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Upcoming Meetings",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    label: "Private Rooms",
+    route: "/private-rooms",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+        1: "#1c1f2e",
+        2: "#161925",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
